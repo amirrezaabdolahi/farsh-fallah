2ce01bac1c5a4cef600f477db6890eb47b3b8595
amirrezaabdolahi <amirenzevadev@gmail.com>
2025-12-07 12:45:14 +0330

    creating products pages ui

---
+++ app/products/page.jsx
@@ -0,0 +1,93 @@
+import PageLayout from '@/components/PageLayout'
+import { DangerousRounded, DataThresholdingRounded, Inventory2Rounded, ShowChartRounded, TrendingDownRounded, TrendingUpRounded, VerifiedRounded } from '@mui/icons-material'
+import { Card, Typography } from '@mui/material'
+import { Box } from '@mui/system'
+import React from 'react'
+
+const Products = () => {
+    return (
+        <PageLayout>
+            <Card className='w-full rounded-xl! overflow-hidden border border-gray-200 flex items-center justify-between shadow-lg px-6 py-4'>
+                <Box className="flex items-center gap-2">
+                    <Box sx={{ backgroundColor: 'success.light', color: 'white', padding: 2, }} className='rounded-2xl' >
+                        <DataThresholdingRounded />
+                    </Box>
+                    <Box>
+                        <Typography variant='subtitle1'>
+                            بیشترین فروش
+                        </Typography>
+                        <Typography variant='body1' className='font-bold!'>
+                            فرش تبریز
+                        </Typography>
+                    </Box>
+                </Box>
+                <Box className="flex items-center gap-2">
+                    <Box sx={{ backgroundColor: 'primary.light', color: 'white', padding: 2, }} className='rounded-2xl' >
+                        <TrendingUpRounded />
+                    </Box>
+                    <Box>
+                        <Typography variant='subtitle1'>
+                            بالاترین سود
+                        </Typography>
+                        <Typography variant='body1' className='font-bold!'>
+                            فرش تبریز
+                        </Typography>
+                    </Box>
+                </Box>
+                <Box className="flex items-center gap-2">
+                    <Box sx={{ backgroundColor: 'error.light', color: 'white', padding: 2, }} className='rounded-2xl' >
+                        <TrendingDownRounded />
+                    </Box>
+                    <Box>
+                        <Typography variant='subtitle1'>
+                            کم ترین فروش
+                        </Typography>
+                        <Typography variant='body1' className='font-bold!'>
+                            فرش کیش
+                        </Typography>
+                    </Box>
+                </Box>
+                <Box className="flex items-center gap-2">
+                    <Box sx={{ backgroundColor: 'success.light', color: 'white', padding: 2, }} className='rounded-2xl' >
+                        <VerifiedRounded />
+                    </Box>
+                    <Box>
+                        <Typography variant='subtitle1'>
+                            موجودی
+                        </Typography>
+                        <Typography variant='body1' className='font-bold!'>
+                            3,000
+                        </Typography>
+                    </Box>
+                </Box>
+                <Box className="flex items-center gap-2">
+                    <Box sx={{ backgroundColor: 'warning.light', color: 'white', padding: 2, }} className='rounded-2xl' >
+                        <DangerousRounded />
+                    </Box>
+                    <Box>
+                        <Typography variant='subtitle1'>
+                            ناموجود
+                        </Typography>
+                        <Typography variant='body1' className='font-bold!'>
+                            26
+                        </Typography>
+                    </Box>
+                </Box>
+            </Card>
+
+            <Box className="flex w-full items-center justify-between">
+                <Box>
+                    <Typography variant='h6'>
+                        محصولات
+                    </Typography>
+                </Box>
+                <Box>
+                    
+                </Box>
+            </Box>
+
+        </PageLayout>
+    )
+}
+
+export default Products
