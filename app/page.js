25c0f69097902add4bbaab89ea73f313ce9b3ad2
amirrezaabdolahi <amirenzevadev@gmail.com>
2025-12-15 23:21:49 +0330

    start to connect to apis first step is dashboard

--- app/page.js
+++ app/page.js
@@ -1,91 +1,146 @@
-
 import AreaChartComponent from "@/components/charts/AreaChartComponent";
 import BarChartView from "@/components/charts/BarChart";
 import ChartFiterComponent from "@/components/charts/ChartFiterComponent";
 import PageLayout from "@/components/PageLayout";
-import { AddCardRounded, AttachMoneyRounded, CalendarMonthRounded } from "@mui/icons-material";
-import { Box, Button, Card, Divider, IconButton, TextField, Typography } from "@mui/material";
+import {
+    AddCardRounded,
+    AttachMoneyRounded,
+    CalendarMonthRounded,
+} from "@mui/icons-material";
+import {
+    Box,
+    Button,
+    Card,
+    Divider,
+    IconButton,
+    TextField,
+    Typography,
+} from "@mui/material";
 
-const cards = [
-  { id: 1, title: "درآمد", value: "۵۰,۰۰۰,۰۰۰ تومان", icon: <AttachMoneyRounded fontSize="inherit" color="inherit" /> },
-  { id: 2, title: "تعداد فروش", value: "23", icon: <AddCardRounded fontSize="inherit" color="inherit" /> },
-  { id: 3, title: "تاریخ", value: "1404/10/20", icon: <CalendarMonthRounded fontSize="inherit" color="inherit" /> },
-  { id: 4, title: "فروش ماهانه", value: "23", icon: <AddCardRounded fontSize="inherit" color="inherit" /> },
+export default async function Home() {
+    const dashBoardData = await fetch(
+        `${process.env.BACKEND_API_URL}api/reports/dashboard/`
+    ).then((res) => res.json());
 
-]
+    const BarChartData = await fetch(
+        `${process.env.BACKEND_API_URL}api/reports/customers_by_region/`
+    ).then((res) => res.json());
 
-const BarChartData = [
-  { area: 1, count: 3 },
-  { area: 2, count: 5 },
-  { area: 3, count: 2 },
-  { area: 4, count: 8 },
-  { area: 5, count: 6 },
-  { area: 6, count: 4 },
-  { area: 7, count: 7 },
-  { area: 8, count: 5 },
-  { area: 9, count: 9 },
-  { area: 10, count: 6 },
-  { area: 11, count: 4 },
-  { area: 12, count: 8 },
-  { area: 13, count: 7 },
-  { area: 14, count: 5 },
-  { area: 15, count: 9 },
-  { area: 16, count: 6 },
-  { area: 17, count: 4 },
-  { area: 18, count: 8 },
-  { area: 19, count: 20 },
-  { area: 20, count: 5 },
-  { area: 21, count: 9 },
-]
+    console.log(BarChartData);
 
+    const cards = [
+        {
+            id: 1,
+            title: "درآمد ماه",
+            value: `${Number(dashBoardData?.month_profit).toLocaleString()}ت`,
+            icon: <AttachMoneyRounded fontSize="inherit" color="inherit" />,
+        },
+        {
+            id: 2,
+            title: "درآمد روز",
+            value: `${dashBoardData?.today_profit}`,
+            icon: <CalendarMonthRounded fontSize="inherit" color="inherit" />,
+        },
+        {
+            id: 3,
+            title: "تعداد فروش روز",
+            value: `${dashBoardData?.today_orders}`,
+            icon: <AddCardRounded fontSize="inherit" color="inherit" />,
+        },
+        {
+            id: 4,
+            title: "تعداد فروش ماه",
+            value: `${dashBoardData?.month_sales}`,
+            icon: <AddCardRounded fontSize="inherit" color="inherit" />,
+        },
+    ];
 
+    return (
+        <PageLayout>
+            <Box className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 items-center justify-between py-2 gap-2 lg:gap-4 ">
+                {cards.map((card) => (
+                    <Card
+                        key={card.id}
+                        className="w-full p-4 bg-white rounded-lg shadow-md relative flex items-center justify-between"
+                    >
+                        <div
+                            className={`absolute w-1 right-0 top-0 bottom-0 bg-blue-400 ${
+                                card.id === 1 ? "block" : "hidden"
+                            }`}
+                        ></div>
+                        <div className="">
+                            <Typography
+                                variant="h6"
+                                className="mb-2 text-gray-700"
+                            >
+                                {card.title}
+                            </Typography>
+                            <Typography
+                                variant="h6"
+                                className="font-bold!"
+                                color="primary.dark"
+                            >
+                                {card.value}
+                            </Typography>
+                        </div>
+                        <IconButton
+                            size=""
+                            className="bg-blue-100 text-blue-500!"
+                        >
+                            {card.icon}
+                        </IconButton>
+                    </Card>
+                ))}
+            </Box>
 
-export default function Home() {
+            {/* bar chart and add product boxs */}
+            <Box className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-6 gap-4 h-150 lg:h-100 mt-4">
+                <Box className="flex flex-col gap-4 ">
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        className="h-full w-full"
+                    >
+                        اضافه کردن فرش
+                    </Button>
+                    <Button
+                        variant="outlined"
+                        color="primary"
+                        className="h-full w-full"
+                    >
+                        مشاهده فرش ها
+                    </Button>
+                </Box>
 
+                <Box className="flex flex-col gap-4">
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        className="h-full w-full"
+                    >
+                        اضافه کردن تابلو
+                    </Button>
+                    <Button
+                        variant="outlined"
+                        color="primary"
+                        className="h-full w-full"
+                    >
+                        مشاهده تابلو ها
+                    </Button>
+                </Box>
 
-  return (
-    <PageLayout>
-      <Box className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 items-center justify-between py-2 gap-2 lg:gap-4 ">
-        {cards.map((card) => (
-          <Card key={card.id} className="w-full p-4 bg-white rounded-lg shadow-md relative flex items-center justify-between">
-            <div className={`absolute w-1 right-0 top-0 bottom-0 bg-blue-400 ${card.id === 1 ? 'block' : 'hidden'}`}></div>
-            <div className="">
-              <Typography variant="h6" className="mb-2 text-gray-700">{card.title}</Typography>
-              <Typography variant="h6" className="font-bold!" color="primary.dark">{card.value}</Typography>
-            </div>
-            <IconButton size="" className="bg-blue-100 text-blue-500!">
-              {card.icon}
-            </IconButton>
-          </Card>
-        ))}
-      </Box>
+                <Box
+                    className="col-span-full lg:col-span-4 shadow-lg border border-gray-100 rounded-lg p-6 "
+                    dir="ltr"
+                >
+                    <BarChartView data={BarChartData} />
+                </Box>
+            </Box>
 
-      {/* bar chart and add product boxs */}
-      <Box className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-6 gap-4 h-150 lg:h-100 mt-4">
-
-        <Box className="flex flex-col gap-4 ">
-          <Button variant="contained" color="primary" className="h-full w-full" >اضافه کردن فرش</Button>
-          <Button variant="outlined" color="primary" className="h-full w-full" >مشاهده فرش ها</Button>
-        </Box>
-
-        <Box className="flex flex-col gap-4">
-          <Button variant="contained" color="primary" className="h-full w-full" >اضافه کردن تابلو</Button>
-          <Button variant="outlined" color="primary" className="h-full w-full" >مشاهده تابلو ها</Button>
-        </Box>
-
-        <Box className="col-span-full lg:col-span-4 shadow-lg border border-gray-100 rounded-lg p-6 " dir="ltr">
-          <BarChartView data={BarChartData} />
-        </Box>
-      </Box>
-
-
-      {/* area chart */}
-      <Box></Box>
-      <Divider className="my-4!" />
-      <AreaChartComponent />
-
-
-
-    </PageLayout>
-  );
+            {/* area chart */}
+            <Box></Box>
+            <Divider className="my-4!" />
+            <AreaChartComponent />
+        </PageLayout>
+    );
 }
--- components/charts/BarChart.jsx
+++ components/charts/BarChart.jsx
@@ -1,31 +1,54 @@
 "use client";
-import { Card, Typography } from '@mui/material';
-import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { Card, Typography } from "@mui/material";
+import {
+    BarChart,
+    Bar,
+    Rectangle,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend,
+    ResponsiveContainer,
+} from "recharts";
 
 const BarChartView = ({ data }) => {
     return (
         <ResponsiveContainer width="100%" height="100%">
-            <BarChart
-                style={{ aspectRatio: 1.618 }}
-                responsive
-                data={data}
-            >
+            <BarChart style={{ aspectRatio: 1.618 }} responsive data={data}>
                 <defs>
                     <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-                        <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-                        <stop offset="95%" stopColor="#8884d8" stopOpacity={0.1} />
+                        <stop
+                            offset="5%"
+                            stopColor="#8884d8"
+                            stopOpacity={0.8}
+                        />
+                        <stop
+                            offset="95%"
+                            stopColor="#8884d8"
+                            stopOpacity={0.1}
+                        />
                     </linearGradient>
                     <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-                        <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
-                        <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+                        <stop
+                            offset="5%"
+                            stopColor="#82ca9d"
+                            stopOpacity={0.8}
+                        />
+                        <stop
+                            offset="95%"
+                            stopColor="#82ca9d"
+                            stopOpacity={0}
+                        />
                     </linearGradient>
                 </defs>
 
                 <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="area" />
-                <YAxis width="auto" dataKey="count" />
+                <XAxis dataKey="region" />
+                <YAxis width="auto" dataKey="customer_count" />
                 <Tooltip content={CustomeTooltip} />
-                <Bar dataKey="count"
+                <Bar
+                    dataKey="customer_count"
                     fillOpacity={1}
                     fill="url(#colorUv)"
                     activeBar={<Rectangle fill="#8884d8" />}
@@ -39,12 +62,16 @@ const CustomeTooltip = ({ active, payload, label }) => {
     if (active && payload && payload.length) {
         return (
             <Card className="bg-white p-4 border border-gray-300 shadow text-end rounded-lg!">
-                <Typography variant='body1' className="font-bold">منطقه : {label}</Typography>
-                <Typography variant='body1' className="text-green-500">تعداد : {payload[0].value}</Typography>
+                <Typography variant="body1" className="font-bold">
+                    منطقه : {label}
+                </Typography>
+                <Typography variant="body1" className="text-green-500">
+                    تعداد : {payload[0].value}
+                </Typography>
             </Card>
         );
     }
     return null;
-}
+};
 
 export default BarChartView;
